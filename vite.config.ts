e9011ec54d55d4ebe56951b8b1a2a3751ff0c6b6
Stef Rave <stef.rave@gmail.com>
2022-09-06 22:01:16 +0200

    inline js and css

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import inlinePlugin from "@molgenis/vite-plugin-inline/src/vite-plugin-inline.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(),inlinePlugin()],
+  build: {
+    rollupOptions: {
+      input: ["./index.html", "@molgenis/vite-plugin-inline/src/loader.ts"],
+      output: {
+        manualChunks: undefined,
+      },
+    }
+  },
   assetsInclude: ['**/*.archimate', '**/*.archimate'],
   base: "",
   server: {
